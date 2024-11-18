06b13627e3501d2ae893685d8c06c46c2d2cdb24
Jack Chuma <jack.chuma@coinbase.com>
2024-11-18 11:26:52 +0700

    use proof length in beacon root derivation loop

--- services/syncer/src/syncer.ts
+++ services/syncer/src/syncer.ts
@@ -131,7 +131,7 @@ export default class Syncer {
     let index = 6434;
     const types = ["bytes32", "bytes32"];
 
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < MOCK_L1_STATE_ROOT_PROOF.length; i++) {
       const preImage = [MOCK_L1_STATE_ROOT_PROOF[i], curr];
 
       if ((index & 1) === 0) {
